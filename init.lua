7f724575dfa74ab04473480d826db57b62e2059b
Rich Tong <1782087+richtong@users.noreply.github.com>
2024-08-28 00:56:54 -0700

    fix: got right init.lua and init.vimrc.vim

--- init.lua
+++ init.lua
@@ -1,1 +1,20 @@
-/Users/rich/ws/git/src/user/rich/dotfiles/common/.config/nvim/init.lua
+-- https://neovim.substack.com/p/switching-from-vimscript-to-lua
+-- https://finnala.dev/blog/a-beginners-guide-to-neovim-configuration
+-- if you have init.vim you are using vimscript
+-- this is init.lua note that you cannot have both
+-- init.vimrc.vim is the legacy script which works for both
+-- vim using syntastic since it is single threaded
+-- nvim using coc for the language processing
+-- lvim uses the lunarvim built in system with LSP etc.
+-- https://www.lunarvim.org/docs/beginners-guide/keybinds-overview
+-- move to init.lua moves us to the built in language processor
+
+-- https://lazy.folke.io/installation
+-- https://neovim.io/doc/user/lua-guide.html
+-- put return code into ./lua/plugins
+-- https://www.lazyvim.org/configuration/plugins
+require("config.lazy")
+
+-- the legacy system is the init.vimrc.vim
+-- needs the path and this is not portable but works for now
+-- vim.cmd.source("~/.config/nvim/init.vimrc.vim")
--- init.vimrc.vim
+++ init.vimrc.vim
@@ -1,1 +1,8 @@
-/Users/rich/ws/git/src/user/rich/dotfiles/common/.config/nvim/init.vimrc.vim
+" Added by install-nvim.sh on Sun Oct 16 07:47:23 AM UTC 2022
+" check for vim-plug install if needed
+" https://github.com/junegunn/vim-plug/issues/739
+let data_dir = has('nvim') ? stdpath('data') . '/site' : '~/.vim'
+
+set runtimepath^=~/.vim runtimepath+=~/.vim/after
+let &packpath = &runtimepath
+source ~/.vimrc
