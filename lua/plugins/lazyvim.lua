1ad095742dd0dfb1673e7a92b014330c120f8470
Rich Tong <1782087+richtong@users.noreply.github.com>
2024-08-28 13:14:42 -0700

    feat: building neovim on lua
    Tired lazyvim and it is just ok, so will continue building

--- lua/plugins/lazyvim.lua
+++ lua/plugins/lazyvim.lua
@@ -1,5 +1,11 @@
+-- https://github.com/folke/lazy.nvim
+-- Based on plugins from https://www.lazyvim.org
+--
+-- https://github.com/neovim/nvim-lspconfig
+--
 return {
   {
     "williamboman/mason.nvim",
+    "neovim/nvim-lspconfig",
   }
 }
